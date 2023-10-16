c59f6dc1dbf8b96337e3d52f3502797c083b79c6
dstoffels <dan.stoffels@gmail.com>
2023-10-16 14:01:47 -0500

    feat: integrate with judge0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-'use client';
-
-import * as React from 'react';
-import { useState, useEffect, useRef } from 'react';
-
-import Editor from '@monaco-editor/react';
-
-const defaultMsg: string = ``;
+import IDE from '../components/IDE/IDE';
 
 export default function Home() {
-	const [code, setCode] = useState<string | undefined>(defaultMsg);
-
-	const editorRef = useRef(null);
-
-	const handleEditorMount = (editor: any, monaco: any) => {
-		editorRef.current = editor;
-	};
-
-	const displayValue = () => {
-		console.log(editorRef?.current?.getValue());
-	};
-
 	return (
 		<main>
-			<h1>dcc ide</h1>
-			<button onClick={displayValue}>Run Code</button>
-			<Editor
-				height="50vh"
-				value={code}
-				onChange={(value: string | undefined) => setCode(value)}
-				onMount={handleEditorMount}
-			/>
+			<IDE />
 		</main>
 	);
 }
